e5e53b80cbc3bac3ce66a2c60a6bdc4484cdf0a1
hanseongseo <seeman94@naver.com>
2022-09-30 10:09:51 +0900

    FrontControllerServletV5.java

---
+++ src/main/java/spring/mvc1/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,88 @@
+package spring.mvc1.web.frontcontroller.v5;
+
+import spring.mvc1.web.frontcontroller.ModelView;
+import spring.mvc1.web.frontcontroller.MyView;
+import spring.mvc1.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import spring.mvc1.web.frontcontroller.v3.controller.MemberListControllerV3;
+import spring.mvc1.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import spring.mvc1.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import spring.mvc1.web.frontcontroller.v4.controller.MemberListControllerV4;
+import spring.mvc1.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+import spring.mvc1.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import spring.mvc1.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "FrontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Object handler = getHandler(request);
+
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+
+        ModelView modelView = adapter.handle(request, response, handler);
+
+        MyView view = viewResolver(modelView);
+        view.render(modelView.getModel(), request, response);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        MyHandlerAdapter a;
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter 를 찾을 수 없습니다." + handler);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+
+    private MyView viewResolver(ModelView mv) {
+        return new MyView("/WEB-INF/views/" + mv.getViewName() + ".jsp");
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
+    }
+
+    private void initHandlerMappingMap() {
+//        V3
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+//        V4
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
+    }
+}
